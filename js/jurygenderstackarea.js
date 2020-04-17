486c31aa254fc2a89d0811584b869710259e1102
dpd28 <deb@fastmail.com>
2020-04-17 10:00:33 -0400

    Loads of updates
    Fixed cssgrid settings - layout Directors section; added photos and set styles; added more copy; still adjusting legend styles and chart information - layout and styles; figured out d3js for tickformat and how to add gridlines

--- js/jurygenderstackarea.js
+++ js/jurygenderstackarea.js
@@ -1,9 +1,9 @@
 function juryStack() {
 
   // set the dimensions and margins of the graph
-  var margin = {top: 60, right: 30, bottom: 30, left: 60},
-      width = 600 - margin.left - margin.right,
-      height = 400 - margin.top - margin.bottom;
+  var margin = {top: 10, right: 30, bottom: 30, left: 40},
+      width = 500 - margin.left - margin.right,
+      height = 393 - margin.top - margin.bottom;
   
   // append the svg object to the body of the page
   var svg = d3.select("figure#genderJuryStack")
@@ -31,12 +31,10 @@ function juryStack() {
       //.range(d3.schemeSet2);
       .range(['#c87ef8','#FCB643']);
 
-  
     //stack the data?
     var stackedData = d3.stack()
       .keys(keys)
       (data)
-  
   
   
     //////////
@@ -72,7 +70,6 @@ function juryStack() {
     .tickFormat(d3.format(".0%")))
     .call(g => g.select(".domain").remove()); // removes the line for axis
 
-
   
     // Add Y axis label:
     svg.append("text")
@@ -81,8 +78,6 @@ function juryStack() {
         .attr("y", -20 )
         // .text("# of baby born")
         .attr("text-anchor", "start")
-  
-
   
   
     //////////
@@ -155,7 +150,6 @@ function juryStack() {
       //   .attr("d", area)
       // }
   
-  
       //////////
       // HIGHLIGHT GROUP //
       //////////
@@ -178,32 +172,32 @@ function juryStack() {
       //////////
   
       // Add one dot in the legend for each name.
-      var size = 20
-      svg.selectAll("myrect")
-        .data(keys)
-        .enter()
-        .append("rect")
-          .attr("x", 400)
-          .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
-          .attr("width", size)
-          .attr("height", size)
-          .style("fill", function(d){ return color(d)})
-          .on("mouseover", highlight)
-          .on("mouseleave", noHighlight)
+      // var size = 20
+      // svg.selectAll("myrect")
+      //   .data(keys)
+      //   .enter()
+      //   .append("rect")
+      //     .attr("x", 400)
+      //     .attr("y", function(d,i){ return 10 + i*(size+5)}) // 100 is where the first dot appears. 25 is the distance between dots
+      //     .attr("width", size)
+      //     .attr("height", size)
+      //     .style("fill", function(d){ return color(d)})
+      //     .on("mouseover", highlight)
+      //     .on("mouseleave", noHighlight)
   
       // Add one dot in the legend for each name.
-      svg.selectAll("mylabels")
-        .data(keys)
-        .enter()
-        .append("text")
-          .attr("x", 400 + size*1.2)
-          .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
-          .style("fill", function(d){ return color(d)})
-          .text(function(d){ return d})
-          .attr("text-anchor", "left")
-          .style("alignment-baseline", "middle")
-          .on("mouseover", highlight)
-          .on("mouseleave", noHighlight)
+      // svg.selectAll("mylabels")
+      //   .data(keys)
+      //   .enter()
+      //   .append("text")
+      //     .attr("x", 400 + size*1.2)
+      //     .attr("y", function(d,i){ return 10 + i*(size+5) + (size/2)}) // 100 is where the first dot appears. 25 is the distance between dots
+      //     .style("fill", function(d){ return color(d)})
+      //     .text(function(d){ return d})
+      //     .attr("text-anchor", "left")
+      //     .style("alignment-baseline", "middle")
+      //     .on("mouseover", highlight)
+      //     .on("mouseleave", noHighlight)
   
   });
   }// end function
